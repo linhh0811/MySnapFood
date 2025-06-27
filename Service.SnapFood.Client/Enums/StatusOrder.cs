8dc6e013079b79c19050e46cde2039b29b015f57
manhdeptraii <155596926+manhdeptraii@users.noreply.github.com>
2025-06-27 22:39:50 +0700

    Clean code

---
+++ Service.SnapFood.Client/Enums/StatusOrder.cs
@@ -0,0 +1,13 @@
+﻿namespace Service.SnapFood.Client.Enums
+{
+    public enum StatusOrder
+    {
+        Pending = 0,  /*-Chờ xác nhận*/
+        Confirmed = 1, /*- Đơn hàng đã được xác nhận bởi hệ thống hoặc người quản lý.*/
+        Shipping = 2, /*- Đơn hàng đã được chuyển cho đơn vị vận chuyển.*/
+        Completed = 3, /*- Đơn hàng đã được giao thành công đến khách hàng.*/
+        Cancelled = 4, /*- Đơn hàng bị hủy bởi khách hàng hoặc người quản lý.*/
+        Activity = 5,
+        InActivity = 6,
+    }
+}
